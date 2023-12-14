f492c6d732457ee49fc08d03cf000b0c1a6e8702
Xiaoran Lai <xiaoran.lai@icr.ac.uk>
2023-12-14 18:10:41 -0500

    Restructured the drug constructor to create single drug only To do: 1. to create combination drug constructor as a superset of drug constructor 2. to add more checking of add new doses to drug

--- src/DrugDoseConstructor.R
+++ src/DrugDoseConstructor.R
@@ -1,70 +1,79 @@
-DrugDose <- setRefClass("DrugDose",
+Drug <- setRefClass("Drug",
     fields = list(
-        drugs = "list"  # A list of drugs with their dose levels and numeric order
+        name = "character",
+        doses = "list"
     ),
     methods = list(
-        initialize = function(dose_info = NULL, num_doses = NULL) {
-            if (!is.null(dose_info)) {
+        initialize = function(drugData = NULL, name = NULL, doseCount = NULL) {
+            if (!is.null(drugData)) {
                 # Initialize from YAML file
-                drugs <<- dose_info
-            } else if (!is.null(num_doses)) {
-                # Initialize from provided number of doses
-                drugs <<- lapply(num_doses, function(dose_num) {
-                    list(DoseLevels = setNames(1:dose_num, 1:dose_num))
-                })
+                name <<- drugData$name
+                doses <<- drugData$doses
+            } else if (!is.null(name) && !is.null(doseCount)) {
+                # Initialize with provided name and a default dose level structure
+                name <<- name
+                doseNames <- paste0("dose", seq_len(doseCount))
+                # Creating a list with these names, initializing with NULL or any default value
+                doses <<- setNames(as.list(seq_len(doseCount)), doseNames)
             } else {
-                # Handle NULL scenario - set drugs to an empty list or a default value
-                drugs <<- list()                
-#                stop("Either dose_info (YAML content) or num_doses must be provided.")
+                # NULL scenario
+                name <<- "Unnamed Drug"
+                doses <<- list()
             }
         },
-        getDoseLabels = function(drug_name) {
-            # Returns the dose labels for a given drug
-            return(names(drugs[[drug_name]]$DoseLevels))
+        getDoseLabels = function() {
+            # Returns the ordered dose labels for a given drug
+            return(names(sort(unlist(doses))))
         },
-        getNumericOrder = function(drug_name, dose_label = NULL) {
+        print = function() {
+            cat("Drug Name:", name, "\nDose Levels:", .self$getDoseLabels(), "\n")
+        },        
+        getDoseLevels = function(doseLabel = NULL) {
             # Validate input
-            if (!drug_name %in% names(drugs)) {
-                stop("Invalid drug name")
-            }
-            if (is.null(dose_label)) {
+            if (is.null(doseLabel)) {
                 # Return all numeric levels for the drug
-                return(drugs[[drug_name]]$DoseLevels)
+                return(as.numeric(sort(unlist(doses))))
             } else {
-                # Return the numeric level for the specified dose label
-                if (!dose_label %in% names(drugs[[drug_name]]$DoseLevels)) {
+                # Return the numeric level for the specified dose labels
+                if (!doseLabel %in% names(doses)) {
                     stop("Invalid dose label")
                 }
-                return(drugs[[drug_name]]$DoseLevels[[dose_label]])
+                return(as.numeric(doses[doseLabel]))
             }
         },
-        addDoseLabel = function(drug_name, new_label = NULL, new_order) {
+        addDose = function(new_label = NULL, new_level) {
             # Validate input
-            if (!drug_name %in% names(drugs)) {
-                stop("Invalid drug name")
-            }
             if (is.null(new_label)) {
                 # Generate default label if not provided
-                existing_labels <- names(drugs[[drug_name]]$DoseLevels)
+                existing_labels <- names(doses)
                 new_label_count <- sum(grepl("newlabel", existing_labels))
                 new_label <- paste("newlabel", new_label_count + 1, sep="")
             }
 
-            # Adjust existing labels' numeric order
-            for (label in names(drugs[[drug_name]]$DoseLevels)) {
-                if (drugs[[drug_name]]$DoseLevels[[label]] >= new_order) {
-                    drugs[[drug_name]]$DoseLevels[[label]] <<- drugs[[drug_name]]$DoseLevels[[label]] + 1
+            # Check if the new level is higher than all existing levels
+            if (all(unlist(doses) < new_level)) {
+                # If so, just add the new dose
+                doses[[new_label]] <<- new_level
+            } else {
+                # Adjust existing labels' numeric order
+                for (label in names(doses)) {
+                    if (doses[[label]] >= new_level) {
+                        doses[[label]] <<- doses[[label]] + 1
+                    }
                 }
-            }
-
             # Add the new label with its order
-            drugs[[drug_name]]$DoseLevels[[new_label]] <<- new_order
+            doses[[new_label]] <<- new_level
+            }
         }        
         # ... (other methods)
     )
 )
 
-# Function to create a new DrugDose object
-createDrugDose <- function(dose_info = NULL, num_doses = NULL) {
-    return(DrugDose$new(dose_info = dose_info, num_doses = num_doses))
+# Function to create a new Drug object
+createDrug <- function(drugData = NULL, name = NULL, doseCount = NULL) {
+    return(
+        lapply(
+            drugData,
+            function(d) Drug$new(drugData = d, name = name, doseCount = doseCount))
+    )
 }
