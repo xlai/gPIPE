8c67704d108ba8b1bbaf88bef27ba648136ea125
Xiaoran Lai <xiaoran.lai@icr.ac.uk>
2023-12-15 19:54:59 -0500

    Added methods for checking validity of a dose configuration from a dosecombi object

--- src/PIPEConstructor.R
+++ src/PIPEConstructor.R
@@ -1,23 +1,23 @@
 PipeEstimator <- setRefClass("PipeEstimator",
     fields = list(
-        drugDose = "DrugDose",  # The DrugDose object
+        drugCombi = "DrugCombi",  # The DrugDose object
         currentConfig = "list",  # Current configuration of dose levels for each drug
         isNonDecreasing = "logical"  # TRUE for non-decreasing, FALSE for non-increasing
     ),
     methods = list(
-        initialize = function(drugDoseObject = NULL, isNonDecreasing = TRUE) {
-            if (is.null(drugDoseObject)) {
+        initialize = function(drugCombiObject = NULL, isNonDecreasing = TRUE) {
+            if (is.null(drugCombiObject)) {
                 # Handle the case where drugDoseObject is NULL
                 # This can be set to a default DrugDose object or keep it as NULL
                 # Example: drugDose <<- DrugDose$new() or drugDose <<- NULL
-                drugDose <<- NULL  # or any other default initialization
+                drugCombi <<- NULL  # or any other default initialization
             } else {
-                drugDose <<- drugDoseObject
+                drugCombi <<- drugCombiObject
             }
             isNonDecreasing <<- isNonDecreasing
             # Initialize the current configuration with NULL for each drug
-            if (!is.null(drugDose)) {
-                currentConfig <<- lapply(drugDose$drugs, function(x) NULL)
+            if (!is.null(drugCombiObject)) {
+                currentConfig <<- lapply(drugCombiObject$doseCombinations, function(x) NULL)
             } else {
                 currentConfig <<- NULL
             }
@@ -31,19 +31,8 @@ PipeEstimator <- setRefClass("PipeEstimator",
                 if (length(gamma) != length(currentConfig)) {
                     return(FALSE)  # Invalid configuration length
                 }
-                for (drug_index in seq_along(gamma)) {
-                    currentNumericOrder <- if (is.null(currentConfig[[drug_index]])) {
-                        if (isNonDecreasing) 1 else max(drugDose$getNumericOrder(names(drugDose$drugs)[drug_index]))
-                    } else {
-                        drugDose$getNumericOrder(names(drugDose$drugs)[drug_index], currentConfig[[drug_index]])
-                    }
-                    
-                    newNumericOrder <- drugDose$getNumericOrder(names(drugDose$drugs)[drug_index], gamma[[drug_index]])
-
-                    if ((isNonDecreasing && newNumericOrder < currentNumericOrder) ||
-                        (!isNonDecreasing && newNumericOrder > currentNumericOrder)) {
-                        return(FALSE)
-                    }
+                else{
+                    return(checkMonotonicity(gamma, increasing = isNonDecreasing))
                 }
                 return(TRUE)
             }
@@ -64,7 +53,75 @@ PipeEstimator <- setRefClass("PipeEstimator",
     )
 )
 
+# Function to slice an array by each dimension using lapply
+slice_array_by_dimension <- function(array) {
+  dimensions <- dim(array)
+  num_dimensions <- length(dimensions)
+
+  # Use lapply to iterate over dimensions
+  all_slices <- lapply(1:num_dimensions, function(dim) {
+    # Use lapply to generate slices for each index in the dimension
+    lapply(1:dimensions[dim], function(i) {
+      index_expr <- lapply(1:num_dimensions, function(x) ifelse(x == dim, i, TRUE))
+      do.call("[", c(list(array), index_expr))
+    })
+  })
+
+  return(all_slices)
+}
+
+checkMonotonicity <- function(gamma, increasing = TRUE) {
+    # Determine the number of dimensions
+    numDims <- max(sapply(gamma, function(x) length(x$levels)))
+    
+    # Find the maximum values for each dimension
+    maxValues <- sapply(1:numDims, function(d) {
+        max(sapply(gamma, function(x) {
+            if (length(x$levels) >= d) x$levels[d] else 0
+        }))
+    })
+    
+    # Create an array to store the gamma values, initialised with 0
+    gammaArray <- array(0, dim = maxValues)
+    
+    # Fill the array with the corresponding validity values
+    for (name in names(gamma)) {
+        item <- gamma[[name]]
+        indices <- item$id
+        validity <- ifelse(item$validity, 1, 0) # Convert TRUE/FALSE to 1/0
+        gammaArray[indices] <- validity
+    }
+    
+    # Pre-compute slices for each dimension
+    slices <- slice_array_by_dimension(gammaArray)
+    
+    # Define the comparison function based on the type of monotonicity
+    compare <- if (increasing) {
+        function(slice1, slice2) { any(slice2 < slice1, na.rm = TRUE) }
+    } else {
+        function(slice1, slice2) { any(slice2 > slice1, na.rm = TRUE) }
+    }
+
+    # Check monotonicity in each dimension
+    for (d in 1:numDims) {
+        for (index in 1:(maxValues[d] - 1)) {
+            if (compare(slices[[d]][[index]], slices[[d]][[index + 1]])) {
+                return(FALSE)
+            }
+        }
+    }
+    
+    return(TRUE)
+}
+
 # Function to create a new PipeEstimator object
 createPipeEstimator <- function(drugDoseObject = NULL, isNonDecreasing = TRUE) {
     return(PipeEstimator$new(drugDoseObject, isNonDecreasing))
+}
+
+loglikelihood <- function(p_posterior, epsilon, weight, pipe_configuration){
+    logLL_vector <- weight*pipe_configuration*(log(epsilon) + log(p_posterior)) +
+        weight*(1 - pipe_configuration)*(log(1 - epsilon) + log(1 - p_posterior))
+
+return(sum(logLL_vector))
 }
