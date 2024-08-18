bd13538ed43c80fb82fd8aa16b4c1e28fd09389b
Xiaoran Lai <xiaoran.lai@icr.ac.uk>
2024-08-18 16:20:06 +0100

    Rewrote epsilon tapering function to allow quadratic or custom tapering functions

--- src/doSimulation.r
+++ src/doSimulation.r
@@ -250,8 +250,24 @@ average_summary_across_simulations <- function(simulation_results, prob_true_lis
 
 average_summary <- average_summary_across_simulations(simulation_results, prob_true_list, 0.3, 0.1)
 
-epsilon_tapering <- function(n_total, n_current, epsilon){
-  epsilon_current <- (n_total - n_current)/(n_total + 1)*0.5 + (n_current + 1)/(n_total + 1)*epsilon
-
+epsilon_tapering <- function(n_total, n_current, epsilon, taper_type = "linear", custom_taper = NULL) {
+  
+  if (!is.null(custom_taper)) {
+    # Use the custom tapering function provided by the user
+    epsilon_current <- custom_taper(n_total, n_current, epsilon)
+  } else {
+    if (taper_type == "linear") {
+      # Linear tapering
+      epsilon_current <- (n_total - n_current) / (n_total + 1) * 0.5 + 
+                         (n_current + 1) / (n_total + 1) * epsilon
+    } else if (taper_type == "quadratic") {
+      # Quadratic tapering: slow down tapering at the beginning
+      alpha <- (n_current / n_total) ^ 2
+      epsilon_current <- (1 - alpha) * 0.5 + alpha * epsilon
+    } else {
+      stop("Unsupported taper_type. Please use 'linear', 'quadratic', or provide a custom taper function.")
+    }
+  }
+  
   return(epsilon_current)
 }
